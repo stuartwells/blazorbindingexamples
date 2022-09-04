bcaf1b2d775c19cb3bea4495fd942870961fdd92
Stuart Wells <stuart.wells@sadler-wells.co.uk>
2022-09-04 18:59:11 +0100

    Add Part 3 - Enum Bindings

---
+++ BlazorBindingExamples03/Pages/EnumBinding.razor.cs
@@ -0,0 +1,22 @@
+﻿using BlazorBindingExamples.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorBindingExamples.Pages
+{
+    public partial class EnumBinding : ComponentBase
+    {
+        public bool Initialised { get; set; } = false;
+        public Model Model { get; private set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Model = new(Guid.NewGuid());
+            Model.Gender = Gender.Female;
+
+            await base.OnInitializedAsync();
+
+            // Set this when fully initialised as we use this to prevent premature rendering
+            Initialised = true;
+        }
+    }
+}
